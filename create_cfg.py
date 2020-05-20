9482aa980d3714bb34304c515ae9dcfd4457c0ab
David Crook <idcrook@users.noreply.github.com>
2020-05-19 20:34:15 -0600

    update for python3

--- create_cfg.py
+++ create_cfg.py
@@ -1,24 +1,23 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import json
 
 # partly based on http://stackabuse.com/how-to-send-emails-with-gmail-using-python/
 
-example_config = [{
-    'gmail': {
-        'gmail_user': 'david.crook@gmail.com',
-        'gmail_password': '',
-        'email_to_list': ['me@gmail.com', 'bill@gmail.com'],
-        'subject': 'print job status'
+example_config = [
+    {
+        'gmail': {
+            'gmail_user': 'david.crook@gmail.com',
+            'gmail_password': '',
+            'email_to_list': ['me@gmail.com', 'bill@gmail.com'],
+            'subject': 'print job status',
+        },
     },
-}, {
-    'motion': {
-        'motion_dir': '/data/motion'
-    }
-}]
+    {'motion': {'motion_dir': '/data/motion'}},
+]
 
 # write json file
 output_json = 'example.json'
 with open(output_json, 'w') as jsonfile:
-    print 'saving to', output_json
+    print('saving to', output_json)
     json.dump(example_config, jsonfile)
